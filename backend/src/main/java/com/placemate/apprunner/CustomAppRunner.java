1ee4d81448d8349f87a1afb3dfbe8a00639abc93
PrakshalJain-9 <prakshaljain99.pj@gmail.com>
2026-06-07 17:47:54 +0530

    Hide Some secret

--- backend/src/main/java/com/placemate/apprunner/CustomAppRunner.java
+++ backend/src/main/java/com/placemate/apprunner/CustomAppRunner.java
@@ -23,61 +23,12 @@ import lombok.Data;
 @Component
 @Data
 @AllArgsConstructor
-public class CustomAppRunner implements ApplicationRunner{
-
-	
-	private SuperAdminService superAdminService;
-	private CollegeService collegeService;
-	private StudentService studentService;
-	private PasswordEncoder encoder;
-	private CacheManager cacheManager;
-	private RefreshTokenService refreshTokenService;
-
-	public void addSuperAdminDetails() {
-		Address address = Address.builder().city("Kota").country("India").state("Rajasthan").street("Raanpur").zipCode("325003").build();
-				
-		College college = College.builder().collegeName("iiitKota").address(address).build();
-		collegeService.saveCollege(college);	
-		SuperAdmin superAdmin = SuperAdmin.builder().college(college).emailId("prakshaljain99.pj@gmail.com")
-								.password(encoder.encode("12345678")).role("SUPERADMIN").build();
-		superAdminService.saveSuperAdmin(superAdmin);
-		
-		
-		Student student = Student.builder().college(college).emailId("2023kucp1121@iiitkota.ac.in").name("Prakshal Jain")
-				.role("ADMIN")
-				.password(encoder.encode("1234"))
-				.rollNumber("2023KUCP1121")
-				.build();
-		
-		Student student2 = Student.builder().college(college).emailId("prakshaljain9.pj@gmail.com").name("PJ")
-				.role("ADMIN")
-				.password(encoder.encode("1234"))
-				.rollNumber("2023KUCP1120")
-				.build();
-		
-		studentService.saveStudent(student);
-		studentService.saveStudent(student2);
-				
-	}
-
-	
-	public void deleteCache() {
-		refreshTokenService.deleteAllToken();
-		cacheManager.getCacheNames().forEach(cacheName -> {
-			Cache cache = cacheManager.getCache(cacheName);
-			cache.clear();
-		});
-	}
-	
-	@Override
+public class CustomAppRunner implements ApplicationRunner{@Override
 	public void run(ApplicationArguments args) throws Exception {
-		try {
-			deleteCache();
-//			addSuperAdminDetails();
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
+	
 	}
+
+	
 	
 }
 
